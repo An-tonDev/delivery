738f65dce089d864eaa0a91be5e4f0b523be8470
anthony <ayomideobaroovooboh@gmail.com>
2025-12-13 16:00:06 +0100

    redid the test data

--- models/userModel.js
+++ models/userModel.js
@@ -78,7 +78,7 @@ const userSchema= new mongoose.Schema(
        address:String,
        updatedAt:{
         type:Date,
-        default: Date.now()
+        default: Date.now
        }
      },
      passwordResetToken: String,
